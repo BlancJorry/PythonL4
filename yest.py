1293fba51635b1878ba82832d3337df32160c998
Blanc Jorry <jorryh2@gmail.com>
2023-11-10 17:58:38 -0500

    png en jpg

--- yest.py
+++ yest.py
@@ -1,1 +1,19 @@
-print('hello')
+from io import BytesIO
+from PIL import Image
+
+def png_to_jpg(infich,outfich):
+    try:
+        with open(infich,'rb') as pngf:
+            pngd = pngf.read()
+    except FileNotFoundError:
+        print(f" nou pa jwen fichier {infich} la")  
+    except IOError as e:
+        print(e)          
+
+    jpgd=pngd[33:]    
+
+    with open(outfich,'wb') as jpgf:
+        jpgf.write(jpgd)
+
+
+png_to_jpg()
