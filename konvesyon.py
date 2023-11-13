9a3feb86639125fa041e0886f6b499e1cfa3d361
NThamie13 <148506897+NThamie13@users.noreply.github.com>
2023-11-13 10:15:29 -1000

    Update konvesyon.py

--- konvesyon.py
+++ konvesyon.py
@@ -1,21 +1,29 @@
 #**FASILITE JPG_TO_PNG**
-def jpg_to_png (infile, outfile) :
-    try:
-        with open(infile,'rb') as jpgf:
-            jpgd = jpgf.read()
-    except FileNotFoundError:
-        print(f"Fichier non trouve", {infile})
-        return
-    except IOError as e:
-        print(e)
+infile = "C:/Users/Thamie/Pictures/Saved Pictures/IMG-20220911-WA0025.jpg"
+outfile = "C:/Users/Thamie/Pictures/Saved Pictures/IMG-20220911-WA0025.png"
 
-    pngd = b'\x89PNG\r\n\x1a\n' + jpgd[2:]
-    try :
-        with open (outfile, 'wb') as pngf:
-            pngf.write(pngd)
-    except IOError as e:
-        print(e)
+extensions_images = ['.jpg']
+
+def jpg_to_png (infile, outfile) :
+    if any(infile.lower().endswith(jpg) for jpg in extensions_images):
+        try:
+            with open(infile,'rb') as jpgf:
+                jpgd = jpgf.read()
+        except FileNotFoundError:
+            print(f"Fichier non trouve", {infile})
+            return
+        except IOError as e:
+            print(e)
+            return
+
+        pngd = b'\x89PNG\r\n\x1a\n' + jpgd
+        try :
+            with open (outfile, 'wb') as pngf:
+                pngf.write(pngd)
+                print("Conversion reussie!")
+        except IOError as e:
+            print(e)
+    else:
+        print("Le fichier n'est pas une image JPG valide")        
     
-infile = "C:\\Users\\Thamie\\Pictures\\Saved Pictures\\IMG-20220911-WA0032.jpg"
-outfile="C:\\Users\\Thamie\\Pictures\\Saved Pictures\\IMG-20220911-WA0032.png"
 jpg_to_png(infile,outfile)
