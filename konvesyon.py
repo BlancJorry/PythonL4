b28b8a91892ecfeec77d985f7f9804a57c679db2
NThamie13 <148506897+NThamie13@users.noreply.github.com>
2023-11-20 08:10:08 -1000

    Update konvesyon.py

--- konvesyon.py
+++ konvesyon.py
@@ -1,29 +1,27 @@
-#**FASILITE JPG_TO_PNG**
-infile = "C:/Users/Thamie/Pictures/Saved Pictures/IMG-20220911-WA0025.jpg"
-outfile = "C:/Users/Thamie/Pictures/Saved Pictures/IMG-20220911-WA0025.png"
-
-extensions_images = ['.jpg']
-
-def jpg_to_png (infile, outfile) :
-    if any(infile.lower().endswith(jpg) for jpg in extensions_images):
-        try:
-            with open(infile,'rb') as jpgf:
-                jpgd = jpgf.read()
-        except FileNotFoundError:
-            print(f"Fichier non trouve", {infile})
-            return
-        except IOError as e:
-            print(e)
-            return
-
-        pngd = b'\x89PNG\r\n\x1a\n' + jpgd
+#kreye klas poum fe konvesyon de JPG_TO_PNG**
+class PngClass :
+    def jpg_to_png(infile, outfile) :
+        extensions_images = ['.jpg']
+        if any(infile.lower().endswith(jpg) for jpg in extensions_images) :
+            try:
+                with open(infile, 'rb') as jpgf:
+                    jpgd = jpgf.read()
+            except FileNotFoundError :
+                print(f"fichier non trouve",{infile})
+                return
+            except IOError as e :
+                print(e)
+        
+        pngd = b'\x89JPG\r\n\x1a\n' + jpgd
         try :
-            with open (outfile, 'wb') as pngf:
-                pngf.write(pngd)
-                print("Conversion reussie!")
-        except IOError as e:
-            print(e)
-    else:
-        print("Le fichier n'est pas une image JPG valide")        
+            with open (outfile, 'wb') as pngf :
+                 pngf.write(pngd)
+                 print("conversion reussie**")
+        except IOError :
+            print("Le fichier n'est pas une image valide")
+
+infile = "C:/Users/Thamie/Desktop/Pict/Pict1.jpg"
+outfile = "C:/Users/Thamie/Desktop/Pict/Pict1.png"
+PngClass.jpg_to_png(infile,outfile)
+
     
-jpg_to_png(infile,outfile)
